ff48d31fc664c3a16569634b83dac7c7a7bf0755
Alex Gaynor <alex.gaynor@gmail.com>
2013-09-26 16:34:36 -0700

    Simplification

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,9 +11,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
+
 
 setuptools.setup(
     setup_requires=['pbr'],
-    pbr=True)
+    pbr=True
+)
