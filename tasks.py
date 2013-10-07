8df63548a438584abf22905cf5711b98548a3ac0
Alex Gaynor <alex.gaynor@gmail.com>
2013-10-07 07:28:27 -0700

    Comment

--- tasks.py
+++ tasks.py
@@ -3,6 +3,9 @@ from invoke import task, run
 
 @task
 def release(version):
+    """
+    Version should be a string like '0.4' or '1.0'
+    """
     run('git tag -s "{}"'.format(version))
     run('python setup.py sdist bdist_wheel')
     run('twine upload/alchimia-{}*'.format(version))
